a2d2ce58141ba96ca33e4c0ec2afe9849404401d
Diyaeddin Habdo <diaddinhabdo@gmail.com>
2025-07-05 14:16:31 +0300

    Added project

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,6 +5,19 @@ const projects = [
       description:
         "A platform to upload, review, and approve internship documents. Instructors and advisors manage the flow.",
         url:"https://drive.google.com/drive/folders/1hCnOsEszLCLSkiRJ9A7q144iZHl2xyGM",
+    },{
+  title: "Simple E-Commerce Website",
+  type: "Web App",
+  description:
+    "Built with ASP.NET Core Web API and React. Implements 3-tier architecture, secure access token auth, Cloudinary image storage, and role-based admin panel.",
+  url: "https://drive.google.com/drive/folders/1D32YjDZWCUPVjGHWOgY-NGi0tjt3xUXO?usp=sharing", 
+}
+,{
+      title: "Cafe Order Management",
+      type: "Mobile App",
+      description:
+        "Waiters take table orders via app; kitchen receives them and cashier processes payments by table number.",
+        url:"https://drive.google.com/drive/folders/1ulk7gu5w375BbXTuRbGNo1hard8282uE",
     },
     {
       title: "Banking System",
@@ -13,13 +26,7 @@ const projects = [
         "A bank simulator with 3-tier architecture. Handles customer management and transactions.",
         url:"https://drive.google.com/drive/folders/1zxbapmPO-SvSp2nwavnTUVmc6vmkv-Zt",
     },
-    {
-      title: "Cafe Order Management",
-      type: "Mobile App",
-      description:
-        "Waiters take table orders via app; kitchen receives them and cashier processes payments by table number.",
-        url:"https://drive.google.com/drive/folders/1ulk7gu5w375BbXTuRbGNo1hard8282uE",
-    },
+    
     {
       title: "Database Backup Service",
       type: "Windows Service",
